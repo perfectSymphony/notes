ab8659f206d7799a38fcd667b2e98360409ba297
perfectSymphony <2066309334@qq.com>
2023-08-24 20:33:11 +0800

    细化项目经理面试题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -348,7 +348,8 @@ module.exports = {
           '/project/需求拆分',
           '/project/敏捷基础思想与实践',
           '/project/大促项目管理课程',
-          '/project/常见工作问题总结',
+          '/project/常见面试问题总结一',
+          '/project/常见面试问题总结二',
           '/project/项目经理常见名词解释'
         ]
       }                                                            
